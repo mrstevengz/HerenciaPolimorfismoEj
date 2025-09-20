1633702f4b895592cde3d3ff6598b624f881418e
mrstevengz <ssequeira@uamv.edu.ni>
2025-09-19 18:59:03 -0600

    Se creo la interfaz Combustible con el metodo recargar, el cual luego es implementado por las subclases Carro y Camion

--- src/models/Camion.java
+++ src/models/Camion.java
@@ -1,6 +1,8 @@
 package models;
 
-public class Camion extends Vehiculo {
+import interfaces.Combustible;
+
+public class Camion extends Vehiculo implements Combustible {
 
     //Constructor de la subclase Camion
     public Camion(String marca, String modelo) {
@@ -12,5 +14,11 @@ public class Camion extends Vehiculo {
     public void mover() {
         System.out.println("El camion de la marca " + getMarca() + " y modelo " + getModelo() + " esta arrancando en Carretera Masaya");
     }
+
+    //Se implementa el metodo recargar de la interfaz Combustible
+    public void recargar() {
+        System.out.println("El camion se ha recargado con gasolina Diesel");
+    }
+
 
 }
--- src/models/Carro.java
+++ src/models/Carro.java
@@ -1,6 +1,8 @@
 package models;
 
-public class Carro extends Vehiculo {
+import interfaces.Combustible;
+
+public class Carro extends Vehiculo implements Combustible {
 
     //Constructor para la subclase carro
     public Carro(String marca, String modelo) {
@@ -10,5 +12,10 @@ public class Carro extends Vehiculo {
     //Se implementa el metodo mover de la clase Vehiculo
     public void mover() {
         System.out.println("El carro de la marca " + getMarca() + " y modelo " + getModelo() + " se esta moviendo en Carretera Masaya");
+    }
+
+    //Se implemento el metodo recargar para el Carro, proveniente de la interfaz Combustible
+    public void recargar() {
+        System.out.println("El carro se ha recargado con combustible Super");
     }
 }
